3285b195e98f2f05bdf120f40a9ebcda41262c76
Emmanuel Balogun <emanuelbalogun@gmail.com>
2023-05-29 23:45:22 -0500

    Handle invalid url bug

--- fetcher.js
+++ fetcher.js
@@ -1,30 +1,32 @@
 const request = require("request");
 const URL = process.argv[2];
 const filePath = process.argv[3];
-const fs = require('fs');
+const fs = require("fs");
 //fetch content from remote server
 
 request(URL, (error, response, body) => {
-if(response.statusCode === 200){
-  const content = body;
+  if (!error && response.statusCode === 200) {
+    const content = body;
 
-fs.writeFile(filePath, content, err => {
-  if (err) {
-    console.error(err);
+    fs.writeFile(filePath, content, (err) => {
+      if (err) {
+        console.error(err);
+        process.exit();
+      } else {
+        var stats = fs.statSync(filePath);
+        var fileSizeInBytes = stats.size;
+        console.log(
+          `Downloaded and saved ${fileSizeInBytes} bytes to ${filePath}`
+        );
+      }
+      // file written successfully
+    });
+  } else {
+    if (error.code === "ENOTFOUND") {
+      console.log(`The URL ${URL}  typed is not valid`);
+    } else {
+      console.log(error);
+    }
   }
-  else{
-     
-    var stats = fs.statSync(filePath);
-    var fileSizeInBytes = stats.size;
-    console.log(`Downloaded and saved ${fileSizeInBytes} bytes to ${filePath}`);
-  }
-  // file written successfully
-});
-}  else{
-  console.log(error);
-}
 });
 //write file to disk
-
-
-
